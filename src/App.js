2b1575dc89d62532b87dd84bc3df24e8585cc36a
JlordA <james.ardery@gmail.com>
2020-12-09 23:24:54 -0500

    got nothing to render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import ImageToggle from './Components/ImageToggle'
 import './App.css';
+
 function App() {
-  return <h1>You Can Do This!</h1>;
+  return (
+    <>
+    <h1>You Can Do This!</h1>;
+    <ImageToggle />
+    </>
+  ) 
+
 }
 
 export default App;
---
+++ src/Components/ImageToggle.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {yes, no} from '../objects'
+// import Objects from '../objects'
+
+class ImageToggle extends React.Component {
+
+    image = () => {
+        // console.log(Object.entries(yes))
+        // console.log(Object.entries(no))
+
+    }
+    
+    clickHandler = () => {
+        if ()
+    }
+        
+    render () {
+        return (
+            <>
+            <h1 onClick={this.clickHandler}>{yes, no}</h1>
+            </>
+        )
+    }
+
+    
+}
+export default ImageToggle
